54af6cac7dbcaf1e58d396450f2a338af421bfe4
sanjana22032005-cmd <sanjana22032005@gmail.com>
2026-07-07 12:32:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="h-screen text-white w-full bg-gray-950">
+     <h1>hello guys i an good</h1>
+    </div>
+  )
+}
+
+export default App
